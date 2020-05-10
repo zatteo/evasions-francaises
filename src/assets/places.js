952445e9d9898b2361aef4d8e824ed7a344925e0
Irwisss <audrey.boehm@zatteo.com>
2020-05-10 19:10:05 +0200

    Ajout des distances

--- src/assets/places.js
+++ src/assets/places.js
@@ -1,53 +1,87 @@
 const places = [{
   label: 'Bali',
+  latitude: -8.3304977,
+  longitude: 115.0906401,
   alternatives: ['Corse', 'Côte d\'Azur'],
 }, {
   label: 'Chutes du Niagara',
+  latitude: 43.0945,
+  longitude: -79.05671,
   alternatives: ['Jura'],
 }, {
   label: 'Grand Canyon',
-  alternatives: [],
+  latitude: 36.0965604,
+  longitude: -112.0984941,
+  alternatives: ['Ocres de Rustrel'],
 }, {
   label: 'Mont Everest',
+  latitude: 27.9878675,
+  longitude: 86.9248308,
   alternatives: ['Mont Blanc'],
 }, {
   label: 'Sahara',
+  latitude: 24.49215,
+  longitude: -12.65625,
   alternatives: ['Dune du Pilat'],
 }, {
   label: 'Darjeeling',
+  latitude: 26.8349002,
+  longitude: 88.3071496,
   alternatives: ['Rocamadour'],
 }, {
   label: 'Autriche',
+  latitude: 47.2000338,
+  longitude: 13.199959,
   alternatives: ['Lac d\'Annecy', 'Chateau de Chantilly'],
 }, {
   label: 'Rome',
+  latitude: 41.89193,
+  longitude: 12.51133,
   alternatives: ['Nîmes'],
 }, {
   label: 'Thaïlande',
+  latitude: 15.5,
+  longitude: 101,
   alternatives: ['Gorges du Verdon', 'Lac de Monteynard','Canyon Vacca','Gouffre de la Fage'],
 }, {
   label: 'Afrique du Sud',
+  latitude: -28.8166236,
+  longitude: 24.991639,
   alternatives: ['Roche de Solutré'],
 }, {
   label: 'Lousiane',
+  latitude: 30.8703881,
+  longitude: -92.007126,
   alternatives: ['Marais Poitevin'],
 }, {
   label: 'Canada',
+  latitude: 60.10867,
+  longitude: -113.64258,
   alternatives: ['Plateau des milles étangs', 'Lac de Gaube','Gorges du Verdon'],
 }, {
   label: 'Équateur',
+  latitude: -1.3397668,
+  longitude: -79.3666965,
   alternatives: ['Gorges de la Dordogne'],
 }, {
   label: 'Bavière',
+  latitude: 49,
+  longitude: 11.5,
   alternatives: ['Strasbourg', 'Colmar'],
 }, {
   label: 'Venise',
+  latitude: 45.4371908,
+  longitude: 12.3345898,
   alternatives: ['Annecy', 'Colmar'],
 }, {
   label: 'Grèces',
+  latitude: 48.6902486,
+  longitude: -1.9495636,
   alternatives: ['Nîmes','Le Donon'],
 }, {
   label: 'Finlande',
+  latitude: 64,
+  longitude: 26,
   alternatives: ['Lac Capitello'],
 }]
 
